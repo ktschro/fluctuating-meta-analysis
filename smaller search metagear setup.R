72b0e51a11617e5996d2d1df76d62039e8f02afa
Katie Schroeder <kschro011@gmail.com>
2022-12-05 12:51:47 -0500

    Update smaller search metagear setup.R

--- smaller search metagear setup.R
+++ smaller search metagear setup.R
@@ -3,24 +3,22 @@
 #query link: https://www.webofscience.com/wos/woscc/summary/e6ee96db-8235-4fa3-aed7-9e4554192089-62b02355/relevance/1
 
 #adding daily temperature range and ectotherm increases search results to 469. query link: https://www.webofscience.com/wos/woscc/summary/7795c949-7c90-42a6-aeaa-8a11298e69bb-62b03535/relevance/1
+#insect AND fluctuat* temperature increases to 1465. query link: https://www.webofscience.com/wos/woscc/summary/c6a5247d-ce31-4c18-9dc2-b766a6b666d1-62f40afa/relevance/1
+  # still doesn't include Carrington 2013 paper that I really think it should
 
 library(metagear) # for GUI and effort distribute and abstract screener
 library(tidyverse) # for compiling csvs
 library(here) # for path management
 
-jensens_metalist <- list.files(path = "/Users/katieschroeder/Documents/GitHub/fluctuating meta analysis/the papers", full.names = TRUE) %>%
-  lapply(read.csv) %>%
-  rbind()
-
-jensens_meta <- as.data.frame(do.call(rbind,jensens_metalist))
-jensens_meta <- jensens_meta %>% rename("ABSTRACT" = "Abstract",
+reading_list <- read.csv("fluct_temp_ectotherm_OR_jensens_temp.csv")
+reading_list <- reading_list %>% rename("ABSTRACT" = "Abstract",
                                         "TITLE" = "Article.Title")
 
-effort_distribute(jensens_meta, initialize = TRUE, reviewers = "katie", save_split = TRUE)
+effort_distribute(reading_list, initialize = TRUE, reviewers = "katie", save_split = TRUE)
 #to split between multiple screeners: reviewers = c("name 1","name 2"). Can use effort to specify weighting of each
 #link to basic vignette to go through these: http://lajeunesse.myweb.usf.edu/metagear/metagear_basic_vignette.html
 
-abstract_screener("effort_katie1.csv",
+abstract_screener("effort_katie.csv",
                   aReviewer = "katie",
-                  theButtons = c("YES","maybe","NO","analyze","read later"),
-                  keyBindingToButtons = c("y","m","n","v","r"))
+                  theButtons = c("YES","maybe","NO","analyze","read"),
+                  keyBindingToButtons = c("y","m","n","t","r"))
