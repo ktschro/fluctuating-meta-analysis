65946d71f001913bf3963a52da244835132e3804
Katie Schroeder <kschro011@gmail.com>
2023-05-19 09:30:34 -0400

    abstract screening

--- april 2023/metagear setup.R
+++ april 2023/metagear setup.R
@@ -6,6 +6,16 @@
 #gets you to 1820+
 # link: https://www.webofscience.com/wos/woscc/summary/3f7a503f-dab6-4c64-93aa-fecc5b82a5d9-85d34eff/relevance/1 
 
+#broadest query option: https://www.webofscience.com/wos/woscc/summary/47bed85c-4d2b-4523-ab6a-2089c2193aa6-8b250fbd/relevance/1 
+
+#10,000 plus results
+
+#refine by some journals - https://www.webofscience.com/wos/woscc/summary/a90c17bb-387d-4575-b2ee-f2e8d92f241a-8b254d09/relevance/1
+
+#6786 results
+
+#((thermal fluct*) OR (temperature fluct*) OR (variable temperature) OR (varying temperature) OR (variable thermal)) AND (growth rate OR develop* rate) AND (ectotherm OR insect OR mosquito* OR algae OR plant OR zooplankt* OR phytoplankt* OR bacteri* OR fung*)
+
 library(metagear) # for GUI and effort distribute and abstract screener
 library(tidyverse) # for compiling csvs
 
@@ -22,4 +32,22 @@ effort_distribute(reading_list, initialize = TRUE, reviewers = "katie", save_spl
 abstract_screener("effort_katie3.csv",
                   aReviewer = "katie",
                   theButtons = c("YES","maybe","NO","analyze","read"),
-                  keyBindingToButtons = c("y","m","n","t","r"))
+                  keyBindingToButtons = c("y","m","n","t","r"),
+                  highlightColor = "powderblue",
+                  highlightKeywords = c("fluctuate","fluctuating","fluctuation","temperature","constant","vary","variable","varying"))
+
+
+################### Second screening option ######### 
+library(tidyverse) # for compiling csvs 
+
+reading_list <- read.csv("Documents/GitHub/fluctuating-meta-analysis/april 2023/savedrecs_may_2023.csv")
+reading_list <- reading_list %>% rename("ABSTRACT" = "Abstract",
+                                        "TITLE" = "Article.Title")
+effort_distribute(reading_list, initialize = TRUE, reviewers = "katie", save_split = TRUE)
+
+#take new effort file (effort_katie.csv) and merge it with effort_katie3.csv (previously categorized papers)
+categorized <- read.csv("effort_katie3.csv")
+new_list <- read.csv("effort_katie.csv")
+
+all_refs <- rbind(new_list,categorized) %>% filter(!duplicated(DOI))
+write.csv(all_refs,"effort_katie3.csv")
---
+++ may 2023 - broad/abstract_screening_may_2023.R
@@ -0,0 +1,39 @@
+setwd("/Users/katieschroeder/Documents/GitHub/fluctuating-meta-analysis/may 2023 - broad")
+
+#broadest query option: https://www.webofscience.com/wos/woscc/summary/47bed85c-4d2b-4523-ab6a-2089c2193aa6-8b250fbd/relevance/1 
+
+#10,000 plus results
+
+#refine by some journals - https://www.webofscience.com/wos/woscc/summary/a90c17bb-387d-4575-b2ee-f2e8d92f241a-8b254d09/relevance/1
+
+#6786 results
+
+#((thermal fluct*) OR (temperature fluct*) OR (variable temperature) OR (varying temperature) OR (variable thermal)) AND (growth rate OR develop* rate) AND (ectotherm OR insect OR mosquito* OR algae OR plant OR zooplankt* OR phytoplankt* OR bacteri* OR fung*)
+
+library(metagear) # for GUI and effort distribute and abstract screener
+library(tidyverse) # for compiling csvs
+
+##do once: combine all csvs from web of science search 
+library(readxl)
+files <- list.files(pattern = '\\.xls')
+tables <- lapply(files,read_excel)
+all_refs <- do.call(rbind,tables)
+write.csv(all_refs,"broad_search_combined_refs.csv")
+
+#do once: initialize reading list
+reading_list <- read.csv("broad_search_combined_refs.csv")
+reading_list <- reading_list %>% rename("ABSTRACT" = "Abstract",
+                                        "TITLE" = "Article.Title")
+
+effort_distribute(reading_list, initialize = TRUE, reviewers = "katie", save_split = TRUE)
+#to split between multiple screeners: reviewers = c("name 1","name 2"). Can use effort to specify weighting of each
+
+#link to basic vignette to go through these: http://lajeunesse.myweb.usf.edu/metagear/metagear_basic_vignette.html
+
+#do every time:
+abstract_screener("effort_katie.csv",
+                  aReviewer = "katie",
+                  theButtons = c("YES","maybe","NO","analyze","read"),
+                  keyBindingToButtons = c("y","m","n","t","r"),
+                  highlightColor = "powderblue",
+                  highlightKeywords = c("fluctuate","fluctuating","fluctuation","temperature","constant","vary","variable","varying"))
